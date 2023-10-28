11e01b15bc9a41744e10a88d9d2a59723b72c085
VinuB-Dev <vinu6th@gmail.com>
2023-10-28 12:30:38 +0530

    workshop solution

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from 'react'
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from './components/CharacterEditor'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
+      <span className='grey-container'></span>
       <CharacterEditor />
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
